10c57ba0e2243085def5ff1295d91ee70af43f72
Leuug <leuugz@gmail.com>
2022-07-19 17:28:09 -0300

    GUIHandler modificações

--- src/GUIHandler.java
+++ src/GUIHandler.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
 import javax.swing.*;
+import cinewise.*;
 
 public class GUIHandler extends JFrame implements ActionListener {
     AnswerGUI answerFrame;
@@ -9,21 +10,25 @@ public class GUIHandler extends JFrame implements ActionListener {
     JLabel score;
     JLabel answerStatus;
     JButton nextPosterButton;
+	CineSession session;
 
     private boolean questionAnswered = true;
     private String NEXTPOSTER = "next_poster";
 
     private String answer = "";
 
-    public GUIHandler() throws IOException {
+	
+    public GUIHandler(int difficulty) throws Exception
+	{
         super("Menu");
+
+		session = new CineSession(difficulty);
 
         this.setLayout(new BorderLayout());
         mainPanel = (JPanel)this.getContentPane();
 
-        score =
-            new JLabel("<html><font size=+2><b>SCORE: ---</b></font></html>",
-                       SwingConstants.CENTER);
+		String scoreLabel = String.format("<html><font size=+2><b>SCORE: %d</b></font></html>", session.getScore());
+        score = new JLabel(scoreLabel, SwingConstants.CENTER);
         mainPanel.add(score, BorderLayout.PAGE_START);
 
         nextPosterButton = new JButton("<html><b>NEXT POSTER</b></html>");
@@ -36,28 +41,64 @@ public class GUIHandler extends JFrame implements ActionListener {
                        SwingConstants.CENTER);
         mainPanel.add(answerStatus, BorderLayout.PAGE_END);
 
-        answerFrame = new AnswerGUI(this);
+        answerFrame = new AnswerGUI(this, session.getMoviePath());
         answerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         answerFrame.setVisible(true);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        if (NEXTPOSTER.equals(e.getActionCommand())) {
-            if (questionAnswered) {
-                answerStatus.setText(
-                    "<html><font size =+1>Your answer is <font color=green>CORRECT</font></html>");
-                score.setText(
-                    "<html><font size=+2><b>SCORE: 3000</b></font></html>");
-                // Get new poster
+    public void actionPerformed(ActionEvent e)
+	{
+        if (NEXTPOSTER.equals(e.getActionCommand()))
+		{
+            if (questionAnswered)
+			{
+				session.nextQuestion();
+				answerStatus.setText("<html><font size =+1>Your answer is ...</font></html>");
+				answer = "";
+				questionAnswered = false;
+				
+				answerFrame.SetMovies(, , , );
+				
+				/*
+				Change poster image
+				answerFrame.SetImage(session.getMoviePath())
+				*/
             }
         }
     }
 
-    public void SetAnswer(String _answer) { answer = _answer; }
+    public void SetAnswer(String _answer)
+	{
+		answer = _answer;
+		questionAnswered = true;
+		try
+		{
+			if (session.evaluate(answer))
+			{
+        		answerStatus.setText("<html><font size =+1>Your answer is <font color=green>CORRECT</font></html>");
+                		
+				session.addToScore(100);
+					
+				score.setText(String.format("<html><font size=+2><b>SCORE: $d</b></font></html>", session.getScore()));
+			}
+			else
+			{
+				answerStatus.setText("<html><font size =+1>Your answer is <font color=red>INCORRECT</font></html>");
+              		
+				session.addToScore(-50);
+				if (session.getScore() < 0)
+					session.addToScore(-1 * session.getScore());
+				
+				score.setText(String.format("<html><font size=+2><b>SCORE: $d</b></font></html>", session.getScore()));
+			}
+		}
+		catch (Exception exception)
+		{}
+	}
 
     public static void main(String[] args) throws Exception {
-        GUIHandler frame = new GUIHandler();
+        GUIHandler frame = new GUIHandler(0);
         frame.setSize(300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
